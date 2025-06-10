b008eed608ea4a87e6be4b5f421cc3efe3cb72c2
soliken <kennethrex456@gmail.com>
2025-06-11 00:05:22 +0800

    Incorrect Controller Used

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,5 @@ use App\Http\Controllers\NameEntryController;
 
 Route::get('/entries', [NameEntryController::class, 'index']);
 Route::post('/entries', [NameEntryController::class, 'store']);
-Route::put('/entries/{id}', [EntryController::class, 'update']);
-Route::delete('/entries/{id}', [EntryController::class, 'destroy']);
+Route::put('/entries/{id}', [NameEntryController::class, 'update']);
+Route::delete('/entries/{id}', [NameEntryController::class, 'destroy']);
