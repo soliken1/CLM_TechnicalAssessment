25cc92710d6bd52e5646e49771787265d71e56bf
soliken <kennethrex456@gmail.com>
2025-06-10 22:37:49 +0800

    Quality Updates

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,5 @@ use App\Http\Controllers\NameEntryController;
 
 Route::get('/entries', [NameEntryController::class, 'index']);
 Route::post('/entries', [NameEntryController::class, 'store']);
+Route::put('/entries/{id}', [EntryController::class, 'update']);
+Route::delete('/entries/{id}', [EntryController::class, 'destroy']);
